3a136fe1892499f2056cd8dc5998c17915656543
Jeremy Legendre <jslegendre@gmail.com>
2018-03-07 23:06:01 -0500

    Setters for NSUserNotification
    While writing the setters I started to think about how/what will be moved into a separate file. I realized that once most of the Obj-C stuff is moved that it would make more sense to keep it all in one place and have the file act as an abstraction layer.  This setup would allow the NS/CF related functionality to sit in a library that can be easily called from C.

--- notify.c
+++ notify.c
@@ -28,18 +28,6 @@ CFStringRef c_cfstr(char * str) {
     return CFStringCreateWithCString(NULL, str, kCFStringEncodingMacRoman);
 }
 
-CFStringRef arg_to_cfstr(char **argv, uintptr_t *i, uintptr_t argc) {
-    char *c_str = calloc(sizeof(char), 150);
-    while(*i != (argc-1) && argv[++*i][0] != '-'){
-        strcat(c_str, argv[*i]);
-        strcat(c_str, " ");
-    }
-    --*i;
-    CFStringRef ret_str = c_cfstr(c_str);
-    free(c_str);    
-    return ret_str;  
-}
-
 void objc_swizzle(Class class, char *sel, Method method) {
     class_replaceMethod(class,
                         sel_registerName(sel),
@@ -63,49 +51,71 @@ id init_notification() {
                             sel_registerName("init"));
 }
 
-void send_notification(CFStringRef title, CFStringRef subtitle, CFStringRef info_text,
-        CFStringRef sound_name)
-{
+void set_title(id notif, char * title) {
+    objc_msgSend(notif, sel_registerName("setTitle:"), c_cfstr(title));
+}
+
+void set_subtitle(id notif, char * subtitle) {
+    objc_msgSend(notif, sel_registerName("setSubtitle:"), c_cfstr(subtitle));
+}
+
+void set_info_text(id notif, char * info_text) {
+    objc_msgSend(notif, sel_registerName("setInformativeText:"), c_cfstr(info_text));
+}
+
+void set_sound_name(id notif, char * sound_name) {
+    objc_msgSend(notif, sel_registerName("setSoundName:"), sound_name);
+}
+
+void post_notification(id notif) {
+    set_bundle_id();
+    id notifCenter = init_notification_center();
+    objc_msgSend(notifCenter, sel_registerName("deliverNotification:"), notif);
+}
+
+void send_notification(char * title, char * subtitle, char * info_text, char * sound_name) {
     id pool = (id)objc_getClass("NSAutoreleasePool");
     
     pool = objc_msgSend(pool,
                         sel_registerName("alloc"),
                         sel_registerName("init"));
     
-    set_bundle_id();
-    id notifCenter = init_notification_center();
+    
     id notif = init_notification();
     
     if (title != NULL) {
-        objc_msgSend(notif, sel_registerName("setTitle:"), title);
-        CFRelease(title);
+        set_title(notif, title);
     }
     if (subtitle != NULL) {
-        objc_msgSend(notif, sel_registerName("setSubtitle:"), subtitle);
-        CFRelease(subtitle);
+        set_subtitle(notif, subtitle);
     }
     if (info_text != NULL) {
-        objc_msgSend(notif, sel_registerName("setInformativeText:"), info_text);
-        CFRelease(info_text);
+        set_info_text(notif, info_text);
     }
     if (sound_name != NULL) {
-        objc_msgSend(notif, sel_registerName("setSoundName:"), sound_name);
-        CFRelease(sound_name);
+        set_sound_name(notif, sound_name);
     }
 
-    objc_msgSend(notifCenter, sel_registerName("deliverNotification:"), notif);
+    post_notification(notif);
     
     sleep(1);
     objc_msgSend(pool, sel_registerName("release"));
 
 }
 
+void concat_args(char *str, char **argv, uintptr_t *i, uintptr_t argc) {
+    while(*i != (argc-1) && argv[++*i][0] != '-'){
+        strcat(str, argv[*i]);
+        strcat(str, " ");
+    }
+    --*i; 
+}
 
 int main(int argc, char** argv) {
-    CFStringRef title = NULL;
-    CFStringRef subtitle = NULL;
-    CFStringRef info_text = NULL;
-    CFStringRef sound_name = NULL;
+    char * title = NULL;
+    char * subtitle = NULL;
+    char * info_text = NULL;
+    char * sound_name = NULL;
 
     // check for piped data
     if ( !isatty(STDIN_FILENO) ) {
@@ -115,25 +125,28 @@ int main(int argc, char** argv) {
             strcat(msg, buf);
         }
         if (msg[strlen(msg)-1] == '\n') {
-            title = c_cfstr("Pipe");
-            info_text = c_cfstr(msg);
+            title = "Pipe";
+            info_text = msg;
         }
     }
 
     if (argc > 1) {
         for (uintptr_t i = 1; i < argc; i++) {
             if ( !strcmp(argv[i], "-title") ) {
-                title = arg_to_cfstr(argv, &i, argc); 
+                title = calloc(sizeof(char), 100);
+                concat_args(title, argv, &i, argc); 
             }
             else if ( !strcmp(argv[i], "-subtitle") ) {
-                subtitle = arg_to_cfstr(argv, &i, argc);          
+                subtitle = calloc(sizeof(char), 150);
+                concat_args(subtitle, argv, &i, argc);          
             }
             else if ( !strcmp(argv[i], "-msg") && info_text == NULL ) {
-                info_text = arg_to_cfstr(argv, &i, argc);
+                info_text = calloc(sizeof(char), 350);
+                concat_args(info_text, argv, &i, argc);
             }
             else if ( !strcmp(argv[i], "-sound") ) {
                 i++;
-                sound_name = c_cfstr(argv[i]);            
+                sound_name = argv[i];            
             }
             else if ( !strcmp(argv[i], "-help") ) {
                 help_msg(argv[0]);            
